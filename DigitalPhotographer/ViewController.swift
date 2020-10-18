83870fa7c9563331b71865de5634435ab5ab9bf9
arbridev <arebrif@gmail.com>
2020-10-18 11:24:08 -0400

    Initial Commit

---
+++ DigitalPhotographer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DigitalPhotographer
+//
+//  Created by Armando Brito on 10/18/20.
+//  Copyright © 2020 arbridev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
