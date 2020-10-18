b857e5b7fb567ebb3084b4ff45fe7926557bc0fb
arbridev <arebrif@gmail.com>
2020-10-18 13:45:50 -0400

    feat: add initial view layouts and navigation
    integrate cocoapods to project (Unsplash, Realm)

---
+++ DigitalPhotographer/Controllers/FavoritesVC.swift
@@ -0,0 +1,30 @@
+//
+//  FavoritesVC.swift
+//  DigitalPhotographer
+//
+//  Created by Armando Brito on 10/18/20.
+//  Copyright © 2020 arbridev. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'DigitalPhotographer' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for DigitalPhotographer
+  pod 'UnsplashPhotoPicker', '~> 1.1.1'
+  pod 'RealmSwift', '5.3.6'
+
+  target 'DigitalPhotographerTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
